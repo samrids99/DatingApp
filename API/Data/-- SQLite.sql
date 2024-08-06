6a4b325ef5f971ab9925ed2bac57429a715e8dce
samrids99 <samridsdale108@gmail.com>
2024-08-06 14:27:33 +0100

    end of section 2

---
+++ API/Data/-- SQLite.sql
@@ -0,0 +1,11 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Dave");
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Jane");
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Jim");
+
+
+
+
