3c205d304d0ed9bd205723a58db8b697b020eb17
samrids99 <samridsdale108@gmail.com>
2024-08-08 11:45:31 +0100

    Hooking up the register method to the service - end of section 5

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -6,18 +7,22 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent implements OnInit {
-  @Input() usersFromHomeComponent: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {}
 
-  constructor() {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
     
   }
 
   register() {
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe({
+      next: () => {
+        this.cancel();
+      },
+      error: error => console.log(error)
+    });
   }
 
   cancel() {
