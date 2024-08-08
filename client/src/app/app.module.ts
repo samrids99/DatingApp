4ad1c69c8fbcf01bc4948f2043ad53e9c57aedde
samrids99 <samridsdale108@gmail.com>
2024-08-08 10:24:56 +0100

    basic home page added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
