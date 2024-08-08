ca185e1c5b5d19fcb59fdbed2f82829606a38b07
samrids99 <samridsdale108@gmail.com>
2024-08-08 11:05:00 +0100

    parent to child communication example

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { error, log } from 'console';
-import { response } from 'express';
 import { AccountService } from './_services/account.service';
 import { User } from './_models/user';
 
@@ -12,20 +9,12 @@ import { User } from './_models/user';
 })
 export class AppComponent implements OnInit{
   title = 'Dating App';
-  users: any;
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
-  }
+  
 
   setCurrentUser() {
     const userString = localStorage.getItem('user'); // could also use:    const user: User = JSON.parse(localStorage.getItem('user')!);
