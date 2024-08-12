ff51e0ee820763da56c3ec42538cb2d59da4e931
samrids99 <samridsdale108@gmail.com>
2024-08-12 10:38:53 +0100

    adding the photo gallery and default user image

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -4,16 +4,18 @@ import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css',
-  imports: [CommonModule, TabsModule]
+  imports: [CommonModule, TabsModule, GalleryModule]
 })
 export class MemberDetailComponent implements OnInit {
   member: Member | undefined;
+  images: GalleryItem[] = [];
 
   constructor(private memberService: MembersService, private route: ActivatedRoute) { }
 
@@ -25,8 +27,18 @@ export class MemberDetailComponent implements OnInit {
     var username = this.route.snapshot.paramMap.get('username');
     if (!username) return;
     this.memberService.getMember(username).subscribe({
-      next: member => this.member = member
+      next: member => {
+        this.member = member,
+        this.getImages()
+      }
     })
+  }
+
+  getImages() {
+    if (!this.member) return;
+    for (const photo of this.member?.photos) {
+      this.images.push(new ImageItem({src: photo.url, thumb: photo.url}));
+    }
   }
 
 }
