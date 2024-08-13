405d6696f58f1b37307ae77892ec180521633eeb
samrids99 <samridsdale108@gmail.com>
2024-08-13 16:56:13 +0100

    auth guard error fix

--- client/src/app/_guards/auth.guard.ts
+++ client/src/app/_guards/auth.guard.ts
@@ -1,21 +1,21 @@
+import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AccountService } from '../_services/account.service';
-import { inject } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
-import { map } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const accountService = inject(AccountService);
   const toastr = inject(ToastrService);
 
   return accountService.currentUser$.pipe(
-    map((user) => {
-      if (user) return true;
-      else {
-        toastr.error('you shall not pass!');
+    map(user => {
+      if (user) {
+        return true;
+      } else {
+        toastr.error('You shall not pass!');
         return false;
       }
     })
-  )
-
+  );
 };
