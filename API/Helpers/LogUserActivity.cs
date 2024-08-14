3f6d11c311b0370a93483540c724a41571cff267
samrids99 <samridsdale108@gmail.com>
2024-08-14 12:47:29 +0100

    made last active action filter more optimal

--- API/Helpers/LogUserActivity.cs
+++ API/Helpers/LogUserActivity.cs
@@ -12,10 +12,10 @@ public class LogUserActivity : IAsyncActionFilter
 
         if(!resultContext.HttpContext.User.Identity.IsAuthenticated) return;
 
-        var username = resultContext.HttpContext.User.GetUsername();
+        var userId = resultContext.HttpContext.User.GetUserId();
 
         var repo = resultContext.HttpContext.RequestServices.GetRequiredService<IUserRepository>();
-        var user = await repo.GetUserByUsernameAsync(username);
+        var user = await repo.GetUserByIdAsync(int.Parse(userId));
         user.LastActive = DateTime.UtcNow;
         await repo.SaveAllAsync();
     }
