d598fc20af79abb898a59717391fa8ac719b6815
samrids99 <samridsdale108@gmail.com>
2024-08-14 12:23:40 +0100

    added sorting functionality

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -57,6 +57,12 @@ public class UserRepository : IUserRepository
         var maxDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge)); // finds the youngest we are looking for
 
         query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+
+        query = userParams.OrderBy switch
+        {
+            "created" => query.OrderByDescending(u => u.Created),
+            _ => query.OrderByDescending(u => u.LastActive)
+        };
     
 
         return await PagedList<MemberDto>.CreateAsync(
