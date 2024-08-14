983c19b6d9284fc284f3311d0520e691516f0ae4
samrids99 <samridsdale108@gmail.com>
2024-08-14 15:57:58 +0100

    controller methods for the likes feature done

---
+++ API/Controllers/LikesController.cs
@@ -0,0 +1,54 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class LikesController : BaseApiController
+{
+
+    private readonly IUserRepository _userRepository;
+    private readonly ILikesRepository _likesRepository;
+    public LikesController(IUserRepository userRepository, ILikesRepository likesRepository)
+    {
+        _likesRepository = likesRepository;
+        _userRepository = userRepository;
+    }
+
+    [HttpPost("{username}")]
+    public async Task<ActionResult> AddLike(string username)
+    {
+        var sourceUserId = int.Parse(User.GetUserId());
+        var likedUser = await _userRepository.GetUserByUsernameAsync(username);
+        var sourceUser = await _likesRepository.GetUserWithLikes(sourceUserId);
+
+        if (likedUser == null) return NotFound();
+
+        if (sourceUser.UserName == username) return BadRequest("you cannot like yourself, you're big headed enough as it is...");
+        var userLike = await _likesRepository.GetUserLike(sourceUserId, likedUser.Id);
+
+        if (userLike != null) return BadRequest("you already liked this user, calm down");
+
+        userLike = new UserLike
+        {
+            SourceUserId = sourceUserId,
+            TargetUserId = likedUser.Id
+        };
+
+        sourceUser.LikedUsers.Add(userLike);
+
+        if (await _userRepository.SaveAllAsync()) return Ok();
+
+        return BadRequest("failed to like user");
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<LikeDto>>> GetUserLikes(string predicate)
+    {
+        var users = await _likesRepository.GetUserLikes(predicate, int.Parse(User.GetUserId()));
+
+        return Ok(users);
+    }
+}
