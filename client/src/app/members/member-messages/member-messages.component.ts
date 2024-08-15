b4325f405a02a085ca054e541697339b9f1a1be8
samrids99 <samridsdale108@gmail.com>
2024-08-15 14:42:13 +0100

    ability to send messages added

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,22 +1,35 @@
-import { Component, input, Input, OnInit } from '@angular/core';
+import { Component, input, Input, OnInit, ViewChild } from '@angular/core';
 import { MessageService } from '../../_services/message.service';
 import { Message } from '../../_models/message';
 import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-member-messages',
   templateUrl: './member-messages.component.html',
   styleUrl: './member-messages.component.css',
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, FormsModule]
 })
 export class MemberMessagesComponent implements OnInit {
+  @ViewChild('messageForm') messageForm?: NgForm;
   @Input() messages: Message[] = [];
   @Input() username?: string;
+  messageContent = '';
 
-  constructor () { }
+  constructor (private messageService: MessageService) { }
 
   ngOnInit(): void {
+  }
+
+  sendMessage() {
+    if (!this.username) return;
+    this.messageService.sendMessage(this.username, this.messageContent).subscribe({
+      next: (r: Message) => {
+        this.messages.push(r);
+        this.messageForm?.reset();
+      }
+    })
   }
 
   
