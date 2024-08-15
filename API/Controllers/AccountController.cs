1a076a7b2c5e251aa6533eea396f01b388667d71
samrids99 <samridsdale108@gmail.com>
2024-08-15 15:38:46 +0100

    setting up identities and role management

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -34,12 +34,7 @@ public class AccountController : BaseApiController
 
     var user = _mapper.Map<AppUser>(registerDto);
 
-    using var hmac = new HMACSHA512(); // using disposes the class once we are done with it
-
     user.UserName = registerDto.Username.ToLower();
-    user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-    user.PasswordSalt = hmac.Key;
-
 
     _context.Users.Add(user);
     await _context.SaveChangesAsync();
@@ -61,14 +56,6 @@ public class AccountController : BaseApiController
 
     if (user == null) return Unauthorized("invalid username.");
 
-    using var hmac = new HMACSHA512(user.PasswordSalt);
-
-    var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-    for (int i = 0; i < computedHash.Length; i++)
-    {
-      if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password.");
-    }
     return new UserDto
     {
       Username = user.UserName,
--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -20,11 +20,7 @@ public class Seed
 
     foreach (var user in users)
     {
-        using var hmac = new HMACSHA512();
-
         user.UserName = user.UserName.ToLower();
-        user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
-        user.PasswordSalt = hmac.Key;
 
         context.Users.Add(user);
     }
