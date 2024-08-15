9ed4165e6c580a09391b8259ba81d96e5bba0fe6
samrids99 <samridsdale108@gmail.com>
2024-08-15 09:43:53 +0100

    set up entities for messages

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -11,6 +11,8 @@ public class DataContext : DbContext
 
     public DbSet<AppUser> Users { get; set; } // name of table in DB when created
     public DbSet<UserLike> Likes { get; set; }
+
+    public DbSet<Message> Messages { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
@@ -29,5 +31,17 @@ public class DataContext : DbContext
         .WithMany(l => l.LikedByUsers)
         .HasForeignKey(s => s.TargetUserId)
         .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Entity<Message>()
+        .HasOne(u => u.Recipient)
+        .WithMany(m => m.MessagesReceived)
+        .OnDelete(DeleteBehavior.Restrict);
+        
+        builder.Entity<Message>()
+        .HasOne(u => u.Sender)
+        .WithMany(m => m.MessagesSent)
+        .OnDelete(DeleteBehavior.Restrict);
+
+
     }
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -22,5 +22,7 @@ public class AppUser
   public List<Photo> Photos { get; set; } = new();
   public List<UserLike> LikedByUsers { get; set; } = new();
   public List<UserLike> LikedUsers { get; set; } = new();
+  public List<Message> MessagesSent { get; set; }
+  public List<Message> MessagesReceived { get; set; }
 
 }
