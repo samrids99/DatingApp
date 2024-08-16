228760f0a97f51e0e1a085690b4180923e058756
samrids99 <samridsdale108@gmail.com>
2024-08-16 09:46:21 +0100

    added policy based authorization

---
+++ API/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AdminController : BaseApiController
+    {
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpGet("users-with-roles")]
+        public ActionResult GetUsersWithRoles()
+        {
+            return Ok("Only admin can see this");
+        }
+
+
+        [Authorize(Policy = "ModeratePhotoRole")]
+        [HttpGet("photos-to-moderate")]
+        public ActionResult GetPhotosForModeration()
+        {
+            return Ok("Admins or moderators can see this");
+        }
+    }
+
+}
--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -32,6 +32,12 @@ namespace API.Extensions
                     };
                 });
 
+            services.AddAuthorization(opt =>
+            {
+                opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+            });
+
             return services;
         }
     }
