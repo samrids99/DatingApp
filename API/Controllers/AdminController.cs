56153170ff88f74e8f4e7c19923cc7fd75fd3240
samrids99 <samridsdale108@gmail.com>
2024-08-16 10:06:47 +0100

    enabling admin to edit user roles

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -33,6 +33,31 @@ namespace API.Controllers
             return Ok(users);
         }
 
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpPost("edit-roles/{username}")]
+        public async Task<ActionResult> EditRoles(string username, [FromQuery]string roles)
+        {
+            if (string.IsNullOrEmpty(roles)) return BadRequest("you must select at least one role");
+
+            var selectedRoles = roles.Split(",").ToArray();
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user == null) return NotFound();
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+            if (!result.Succeeded) return BadRequest("couldn't edit roles");
+
+            result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if (!result.Succeeded) return BadRequest("failed to remove from roles");
+
+            return Ok(await _userManager.GetRolesAsync(user));
+
+        }
+
 
         [Authorize(Policy = "ModeratePhotoRole")]
         [HttpGet("photos-to-moderate")]
