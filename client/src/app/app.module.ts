4f72eedb7cb5cd7456e7ddcc275448382f06b9aa
samrids99 <samridsdale108@gmail.com>
2024-08-16 10:47:41 +0100

    added a custom directive to hide admin tab to standard members

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
 import { TextInputComponent } from './_forms/text-input/text-input.component';
 import { DatePickerComponent } from './_forms/date-picker/date-picker.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { HasRoleDirective } from './_directives/has-role.directive';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,8 @@ import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
     PhotoEditorComponent,
     TextInputComponent,
     DatePickerComponent,
-    AdminPanelComponent
+    AdminPanelComponent,
+    HasRoleDirective
   ],
   imports: [
     BrowserModule,
