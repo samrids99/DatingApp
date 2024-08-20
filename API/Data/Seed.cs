30c0861fa6679392f901a60be642ee28d95c48d9
samrids99 <samridsdale108@gmail.com>
2024-08-20 15:57:18 +0100

    changing the DB server in the app

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -7,6 +7,11 @@ namespace API.Data;
 
 public class Seed
 {
+  public static async Task ClearConnections(DataContext context)
+  {
+    context.Connections.RemoveRange(context.Connections);
+    await context.SaveChangesAsync();
+  }
   public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
   {
     if (await userManager.Users.AnyAsync()) return;
@@ -33,6 +38,8 @@ public class Seed
     {
       user.Photos.First().IsApproved = true;
       user.UserName = user.UserName.ToLower();
+      user.Created = DateTime.SpecifyKind(user.Created, DateTimeKind.Utc);
+      user.LastActive = DateTime.SpecifyKind(user.LastActive, DateTimeKind.Utc);
 
       await userManager.CreateAsync(user, "Pa$$w0rd");
 
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -14,7 +14,7 @@ namespace API.Extensions
         {
             services.AddDbContext<DataContext>(opt =>
             {
-                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+                opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
             });
 
             services.AddScoped<ITokenService, TokenService>();
